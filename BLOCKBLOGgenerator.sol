0a738d0a4f8e90024e519b865298c95cd4340e2a
maxxflyer <maxxflyer@gmail.com>
2017-01-09 11:10:19 +0100

    Update

--- BLOCKBLOGgenerator.sol
+++ BLOCKBLOGgenerator.sol
@@ -26,31 +26,26 @@ selfdestruct(owner);
 
 }
 
-
-
-
-pragma solidity ^0.4.6;
-
 contract BLOCKBLOG{
 address public owner; //standard needed for Alpha Layer and generic augmentation
 
 //creation
 function BLOCKBLOG(address o) {
 owner=o;
-logs.push(log(o,"Created Blog","1.0","",block.number));
+logs.push(log("Created Blog","1.0","",o,block.number));
 }
 
 //add a new post at the end of the log
 function addPost(string title,string content,string media,address ethlink) returns(bool){
 if(msg.sender!=owner)throw;
-logs.push(log(ethlink,title,content,media,block.number));
+logs.push(log(title,content,media,ethlink,block.number));
 return true;
 }
 
 //edit a specific post at a given index
 function editPost(uint index,string title,string content,string media,address ethlink) returns(bool){
 if(msg.sender!=owner)throw;
-logs[index]=log(ethlink,title,content,media,block.number);
+logs[index]=log(title,content,media,ethlink,block.number);
 return true;
 }
 
@@ -58,24 +53,25 @@ return true;
 log[] logs;
 
     struct log{
-            address ethlink;
     string title;
     string content;
     string media;
-
+    address ethlink;
     uint blocknumber;
    }
 
 //read the logs by index
-function readLog(uint i)constant returns(uint,address,string,string,string,uint){
+function readLog(uint i)constant returns(uint,string,string,string,address,uint){
 log l=logs[i];
-return(logs.length,l.ethlink,l.title,l.content,l.media,l.blocknumber);
+uint u=logs.length;
+uint b=l.blocknumber;
+return(u,l.title,l.content,l.media,l.ethlink,b);
 }
 
 //change owner
 function manager(address o)returns(bool){
 if(msg.sender!=owner)throw;
-logs.push(log(o,"Owner changed:","","",block.number));
+logs.push(log("Owner changed:","","",o,block.number));
 owner=o;
 return true;
 }
