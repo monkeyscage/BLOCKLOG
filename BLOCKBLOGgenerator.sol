ea8a04c36bd67a234775e88d63d73d04d1478467
maxxflyer <maxxflyer@gmail.com>
2017-01-10 14:08:46 +0100

    added link

--- BLOCKBLOGgenerator.sol
+++ BLOCKBLOGgenerator.sol
@@ -32,20 +32,20 @@ address public owner; //standard needed for Alpha Layer and generic augmentation
 //creation
 function BLOCKBLOG(address o) {
 owner=o;
-logs.push(log("Created Blog","1.0","",o,block.number));
+logs.push(log("Created Blog","1.0","","",o,block.number));
 }
 
 //add a new post at the end of the log
-function addPost(string title,string content,string media,address ethlink) returns(bool){
+function addPost(string title,string content,string media,string link,address ethlink) returns(bool){
 if(msg.sender!=owner)throw;
-logs.push(log(title,content,media,ethlink,block.number));
+logs.push(log(title,content,media,link,ethlink,block.number));
 return true;
 }
 
 //edit a specific post at a given index
-function editPost(uint index,string title,string content,string media,address ethlink) returns(bool){
+function editPost(uint index,string title,string content,string media,string link,address ethlink) returns(bool){
 if(msg.sender!=owner)throw;
-logs[index]=log(title,content,media,ethlink,block.number);
+logs[index]=log(title,content,media,link,ethlink,block.number);
 return true;
 }
 
@@ -56,16 +56,17 @@ log[] logs;
     string title;
     string content;
     string media;
+    string link;
     address ethlink;
     uint blocknumber;
    }
 
 //read the logs by index
-function readLog(uint i)constant returns(uint,string,string,string,address,uint){
+function readLog(uint i)constant returns(uint,string,string,string,string,address,uint){
 log l=logs[i];
 uint u=logs.length;
 uint b=l.blocknumber;
-return(u,l.title,l.content,l.media,l.ethlink,b);
+return(u,l.title,l.content,l.media,l.link,l.ethlink,b);
 }
 
 //change owner
